7fc2873b3752b13e51c18a68f8da1e8825fbc9ce
Rad <chihaby@gmail.com>
2020-01-12 10:15:42 -0800

    puts images in  own component

--- src/components/GetData.js
+++ src/components/GetData.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { ImageDiv } from "./ImageDiv";
 import axios from "axios";
 import styled from "styled-components";
 
@@ -14,20 +15,20 @@ export const Button = styled.button`
 `;
 
 export const RdmButton = styled.button`
-height: 30px;
-width: 100px;
-background: palevioletred;
-border-radius: 3px;
-border: none;
-color: white;
-background: -o-repeating-linear-gradient();
-margin: 10px 10px 20px 10px;
+    height: 30px;
+    width: 100px;
+    background: palevioletred;
+    border-radius: 3px;
+    border: none;
+    color: white;
+    background: -o-repeating-linear-gradient();
+    margin: 10px 10px 20px 10px;
 `;
 
 class GetData extends Component {
     state = {
-        searchInput: '',
-        buttonValue: '',
+        searchInput: "",
+        buttonValue: "",
         rdmButtonvalues: [],
         searchResult: [],
         list: []
@@ -38,7 +39,7 @@ class GetData extends Component {
             const list = res.data.message;
             this.setState({ list });
             this.randomizeButtonValues();
-        });  
+        });
     };
 
     handleChange = event => {
@@ -55,57 +56,59 @@ class GetData extends Component {
 
         if (searchInput) {
             axios
-                .get(`https://dog.ceo/api/breed/${ searchInput }/images`)
+                .get(`https://dog.ceo/api/breed/${searchInput}/images`)
                 .then(res => {
                     const searchResult = res.data.message.slice(0, 9);
                     this.setState({ searchResult });
-            })
-        } 
+                });
+        }
 
         if (buttonValue) {
             axios
-                .get(`https://dog.ceo/api/breed/${ buttonValue }/images`)
+                .get(`https://dog.ceo/api/breed/${buttonValue}/images`)
                 .then(res => {
                     const searchResult = res.data.message.slice(0, 9);
                     this.setState({ searchResult });
-            })
+                });
         }
-    }
+    };
 
     randomizeButtonValues = () => {
-            let rdmButtonvalues = []
-            for (var i=0; i < 9; i++) {
-                var rdmNumber = Object.keys(this.state.list)[Math.floor(Math.random()*Object.keys(this.state.list).length)];
-                rdmButtonvalues.push(rdmNumber);
-            }
-            this.setState({ rdmButtonvalues });
-    }
+        let rdmButtonvalues = [];
+        for (var i = 0; i < 9; i++) {
+            var rdmNumber = Object.keys(this.state.list)[
+                Math.floor(Math.random() * Object.keys(this.state.list).length)
+            ];
+            rdmButtonvalues.push(rdmNumber);
+        }
+        this.setState({ rdmButtonvalues });
+    };
 
     render() {
-        const { searchResult ,list, rdmButtonvalues } = this.state;
+        const { searchResult, list, rdmButtonvalues } = this.state;
         return (
             <div>
                 <div>
                     <form
-                            style={{
-                                textAlign: "center",
-                                marginTop: "20px",
-                                marginBottom: "20px"
-                            }}
-                            onSubmit={this.handleSubmit}
-                        >
-                            <input
-                                type="text"
-                                name="name"
-                                onChange={this.handleChange}
-                            />
-                            <button className="search-button" type="submit">
-                                Submit
-                            </button>
-                        </form>
+                        style={{
+                            textAlign: "center",
+                            marginTop: "20px",
+                            marginBottom: "20px"
+                        }}
+                        onSubmit={this.handleSubmit}
+                    >
+                        <input
+                            type="text"
+                            name="name"
+                            onChange={this.handleChange}
+                        />
+                        <button className="search-button" type="submit">
+                            Submit
+                        </button>
+                    </form>
                 </div>
 
-                <div>     
+                <div>
                     <h1>Constant list</h1>
                     {Object.keys(list)
                         .map(breed => (
@@ -115,37 +118,25 @@ class GetData extends Component {
                 </div>
 
                 <div>
-                    <button onClick={this.randomizeButtonValues}>Click for random list</button>
+                    <button onClick={this.randomizeButtonValues}>
+                        Click for random list
+                    </button>
                 </div>
 
-                <div>     
+                <div>
                     {rdmButtonvalues
                         .map(breed => (
-                            <RdmButton onClick={this.handleSubmit}>{breed}</RdmButton>
+                            <RdmButton onClick={this.handleSubmit}>
+                                {breed}
+                            </RdmButton>
                         ))
                         .slice(1, 9)}
                 </div>
 
-                <div>
-                    <h1>List of images</h1>
-                    {searchResult.map(oneSearch => {
-                        return (
-                            <img
-                                src={oneSearch}
-                                alt={""}
-                                style={{
-                                    height: "100px",
-                                    width: "100px",
-                                    borderRadius: "20%"
-                                    }}
-                            />
-                        );
-                    })}
-                </div>
+                <ImageDiv searchResult={this.state.searchResult} />
             </div>
         );
     }
 }
-
 
 export default GetData;
