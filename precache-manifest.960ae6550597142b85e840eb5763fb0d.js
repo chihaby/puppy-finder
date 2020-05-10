2b7d07ee2a6b0299650059cfa6ed5b18a8cfcaf6
Rad <chihaby@gmail.com>
2020-05-10 12:09:15 -0700

    Updates

---
+++ precache-manifest.960ae6550597142b85e840eb5763fb0d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a9aa3abb052c7e955ba3a74f5f45e83",
+    "url": "/puppy-finder/index.html"
+  },
+  {
+    "revision": "3551441911e0ddf0a64c",
+    "url": "/puppy-finder/static/css/main.80afa655.chunk.css"
+  },
+  {
+    "revision": "334d3bce9de3cf4da062",
+    "url": "/puppy-finder/static/js/2.8ffbc14a.chunk.js"
+  },
+  {
+    "revision": "2097bb7f3af03aec4af35c5376edfb5e",
+    "url": "/puppy-finder/static/js/2.8ffbc14a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3551441911e0ddf0a64c",
+    "url": "/puppy-finder/static/js/main.7a2639ee.chunk.js"
+  },
+  {
+    "revision": "39582ae75aab36729f25",
+    "url": "/puppy-finder/static/js/runtime-main.db1c14a3.js"
+  }
+]);
