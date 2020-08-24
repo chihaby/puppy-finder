f6d4e6af69f9c9aa61155985628f65ad06b022fc
Rad <chihaby@gmail.com>
2020-08-23 19:53:37 -0700

    Updates

---
+++ precache-manifest.85db7303d713d809e243c45b06fe64c1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a66c9560e53a351d2ff9acea0f752163",
+    "url": "/index.html"
+  },
+  {
+    "revision": "76758ec9de7a4d27751e",
+    "url": "/static/css/main.0106c57b.chunk.css"
+  },
+  {
+    "revision": "f6614d982b708fe87039",
+    "url": "/static/js/2.6df3a993.chunk.js"
+  },
+  {
+    "revision": "7070a011b36f3af18e58e3a860926d94",
+    "url": "/static/js/2.6df3a993.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76758ec9de7a4d27751e",
+    "url": "/static/js/main.240a73fd.chunk.js"
+  },
+  {
+    "revision": "61d1e29649076d3b3d4a",
+    "url": "/static/js/runtime-main.6378bd4a.js"
+  },
+  {
+    "revision": "f0ba4a25a3fb0909a8b37c8c90dff250",
+    "url": "/static/media/alldogs.f0ba4a25.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/puppy-finder/precache-manifest.092d06f638ec91b6e72aa1ae36438472.js"
+  "/precache-manifest.85db7303d713d809e243c45b06fe64c1.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/puppy-finder/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
